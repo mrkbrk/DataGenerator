f825dace24b4c9ea42f66691f380f38bfa4b718a
marek <marek@linuxmint.linuxmint-domain>
2013-01-12 03:19:17 +0100

    Changes in generator adding contorl for hash

--- sk/marek/barak/Generator.py
+++ sk/marek/barak/Generator.py
@@ -2,6 +2,24 @@
 import sys
 import csv
 import random
+import datetime
+
+def getDayStringFromInt(day):
+    if day == 0:
+        return "Pondelok"
+    elif day ==1:
+        return "Utorok"
+    elif day == 2:
+        return "Streda"
+    elif day == 3:
+        return "Stvrtok"
+    elif day ==4 :
+        return "Piatok"
+    elif day ==5:
+        return "Sobota"
+    elif day == 6:
+        return "Nedela"
+    else: return ""
 
 def getRandomRow(lst):
     row = []
@@ -17,7 +35,7 @@ def getRandomRow(lst):
 
 def errorChance(size):
     actual = random.randint(0,size)
-    chance = size*0.95
+    chance = size*0
     if actual>chance:
         return True
     else: return False
@@ -55,7 +73,7 @@ def main():
     option = (sys.argv[3])
     print option
     if (option == "m"):
-        finalList = []
+        finalList = list()
         hasList = list()
         for i in range(0,count):
             randomRow = getRandomRow(lst)
@@ -66,41 +84,64 @@ def main():
         finalList.insert(0, header)
         writeItemsToCsv(finalList)
         print "Writen to out.csv"
-    else:
-        
+    elif option == "i":
+        print "Index"
         finalList = list()
         size = len(lst)
         hasList = list()
         for i in range(size):
             indexList = list()
             for j in range(len(header)):
-                if header[j].lower() == "id_cas":
-                    rand = random.randint(1,100)
+                if header[j] == "id_cas":
+                    rand = random.randint(1,1000)
                     indexList.append(rand)
-                elif header[j].lower() =="id_zamestnanec":
-                    rand = random.randint(1,100)
-                    indexList.append(rand)
-                elif header[j].lower() == "id_zakaznik":
-                    rand = random.randint(1,100)
-                    indexList.append(rand)
-                elif header[j].lower() == "id_tovaru":
-                    rand = random.randint(1,100)
-                    indexList.append(rand)
-                elif header[j].lower() == "id_dodavatel":
-                    rand = random.randint(1,100)
-                    indexList.append(rand)
-                elif header[j].lower() == "id_faktura":
-                    rand = random.randint(1,100)
-                    indexList.append(rand)
-            header.reverse()
+                elif header[j] =="id_zamestnanec":
+                        rand = random.randint(1,283)
+                        indexList.append(rand)
+                elif header[j] == "id_zakaznik":
+                        rand = random.randint(1,534)
+                        indexList.append(rand)
+                elif header[j] == "id_tovar":
+                        rand = random.randint(1,451)
+                        indexList.append(rand)
+                elif header[j] == "id_dodavatel" or header[j]=="vybavuje_dodavatel":
+                        rand = random.randint(1,70)
+                        indexList.append(rand)
+                elif header[j] == "id_faktura":
+                        rand = random.randint(1,1996)
+                        indexList.append(rand)
+                elif header[j] == "id_pobocka":
+                        rand = random.randint(1,12)
+                        indexList.append(rand)
+            
             l = lst.pop(0)
             l.pop(0)
-            indexList.extend(l)
             if not hasList.__contains__(str(indexList)):
                 hasList.append(str(indexList))
+                s = list()
+                for i in l:
+                    if i != "":
+                        s.append(i) 
+                indexList.extend(s)
                 finalList.append(indexList)
+                
+                
         finalList.insert(0, header)
         writeItemsToCsv(finalList)
+    elif option == "d":
+        dt = datetime.datetime.now()
+        dates = list()
+        for i in range(count):
+            dt += datetime.timedelta(days=1)
+            today = list()
+            today.append(dt)
+            today.append(dt.year)
+            today.append(dt.month)
+            today.append(dt.day)
+            today.append(getDayStringFromInt(dt.weekday()))
+            dates.append(today)
+        writeItemsToCsv(dates)
+        
         
 if __name__ == '__main__':
     main()
